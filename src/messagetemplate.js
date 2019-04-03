76d3310829fce6585dfffe8f904a8f86328fa826
2kindsofcs <2kindsofcs@gmail.com>
2019-04-03 21:12:38 +0900

    Make stringfy function

--- src/messagetemplate.js
+++ src/messagetemplate.js
@@ -1,62 +1,77 @@
+/**
+ * Stringify user id list with ', ' glue
+ * @param {string[]} userIds
+ * @return {string} stringified user id list
+ */
+function stringifyUserIdList(userIds) {
+  return userIds.map((id) => `<@${id}>`).join(', ');
+}
+
 module.exports = {
-	botMessage: (attendList, absentList, remindList) => {
-		let message = [
-			{
-				"type": "section",
-				"fields": [
-					{
-						"type": "mrkdwn",
-						"text": "*수요조사 나왔습니다*\n이번주 수요일 저녁 8시 모각코 참석하시는 분은 \"참석\"버튼 눌러주세요."
-					}
-				]
-			},
-			{
-				"type": "actions",
-				"elements": [
-					{
-						"type": "button",
-						"text": {
-							"type": "plain_text",
-							"emoji": true,
-							"text": "참석"
-						},
-						"value": "attend"
-					},
-					{
-						"type": "button",
-						"text": {
-							"type": "plain_text",
-							"emoji": true,
-							"text": "불참"
-						},
-						"value": "absent"
-					}
-				]
-			},
-			{
-				"type": "section",
-				"fields": [
-					{
-						"type": "mrkdwn",
-						"text": `참석자: ${attendList.map((id) => `<@${id}>`).join(", ")} 불참: ${absentList.map((id) => `<@${id}>`).join(", ")}`
-					}
-				]
-			}
-		];
-		if (remindList.length > 0) {
-			message.push(
-				{
-					"type": "section",
-					"fields": [
-						{
-							"type": "mrkdwn",
-							"text": `투표안하신분 투표해주세요: ${remindList.map((id) => `<@${id}>`).join(", ")}`
-						}
-					]
-				}
-			)
-		}
-		return message
-	}
+  botMessage: (attendList, absentList, remindList) => {
+    const message = [
+      {
+        'type': 'section',
+        'fields': [
+          {
+            'type': 'mrkdwn',
+            'text': '*수요조사 나왔습니다*\n이번주 수요일 저녁 8시 모각코 참석하시는 분은 "참석"버튼 눌러주세요.',
+          },
+        ],
+      },
+      {
+        'type': 'actions',
+        'elements': [
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': '참석',
+            },
+            'value': 'attend',
+          },
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'emoji': true,
+              'text': '불참',
+            },
+            'value': 'absent',
+          },
+        ],
+      },
+      {
+        'type': 'section',
+        'fields': [
+          {
+            'type': 'mrkdwn',
+            'text': `참석자: ${
+              stringifyUserIdList(attendList)
+            } 불참: ${
+              stringifyUserIdList(absentList)
+            }`,
+          },
+        ],
+      },
+    ];
+    if (remindList.length > 0) {
+      message.push(
+          {
+            'type': 'section',
+            'fields': [
+              {
+                'type': 'mrkdwn',
+                'text': `투표안하신분 투표해주세요: ${
+                  stringifyUserIdList(remindList)
+                }`,
+              },
+            ],
+          }
+      );
+    }
+    return message;
+  },
 };
 
